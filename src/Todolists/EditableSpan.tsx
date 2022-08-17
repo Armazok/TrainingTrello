ed8a959cb04887875070b88df2fdd3c7d2817558
Armazok <nikolai.borsin@mail.ru>
2022-08-18 03:51:51 +0500

    new component EditableSpan

--- src/Todolists/EditableSpan.tsx
+++ src/Todolists/EditableSpan.tsx
@@ -1,11 +1,35 @@
-import React from "react";
+import React, {ChangeEvent, useState} from "react";
 
 type EditableSpanType = {
     title: string
+    onChange: (newValue: string) => void
 }
 
 export function EditableSpan(props: EditableSpanType) {
+    let [editMode, setEditMode] = useState(false)
+    let [title, setTitle] = useState("")
+
+
+    const activeEditMode = () => {
+        setEditMode(true)
+        setTitle(props.title)
+    }
+    const activeViewMode = () => {
+        setEditMode(false)
+        props.onChange(title)
+    }
+
+    const onChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
+        setTitle(e.currentTarget.value)
+    }
+
     return (
-        <span>{props.title}</span>
+        editMode
+            ? <input
+                value={title}
+                onChange={onChangeInput}
+                onBlur={activeViewMode}
+                autoFocus/>
+            : <span onDoubleClick={activeEditMode}>{props.title}</span>
     )
 }
