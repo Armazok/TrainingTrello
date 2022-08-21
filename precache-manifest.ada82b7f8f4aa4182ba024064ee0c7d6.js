f83dc9bc3b58ebe397bd0e2853d901b776cf91da
Armazok <nikolai.borsin@mail.ru>
2022-08-21 22:43:35 +0500

    Updates

---
+++ precache-manifest.ada82b7f8f4aa4182ba024064ee0c7d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26d59b551d201f82bcdf1c48fbd15ef0",
+    "url": "/TrainingTrello/index.html"
+  },
+  {
+    "revision": "3e2774902b30f6a11ef2",
+    "url": "/TrainingTrello/static/css/main.65d373c4.chunk.css"
+  },
+  {
+    "revision": "d1366618bc796528f506",
+    "url": "/TrainingTrello/static/js/2.751e2d48.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/TrainingTrello/static/js/2.751e2d48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e2774902b30f6a11ef2",
+    "url": "/TrainingTrello/static/js/main.438abced.chunk.js"
+  },
+  {
+    "revision": "def24cc854e43f0544b1",
+    "url": "/TrainingTrello/static/js/runtime-main.87fc4735.js"
+  }
+]);
